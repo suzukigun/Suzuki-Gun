3cb4c30adcd47cb1078459e21f5a9b3a00c262b4
Gun Suzuki <gunsuzuki54@gmail.com>
2023-02-16 18:40:36 +0900

    Webサービス　DBと接続

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,5 @@ class UsersController < ApplicationController
         @users = User.all
     end
   
-    private
-      def user_admin
-         @users = User.all
-         if  current_user.admin == false
-             redirect_to root_path
-         else
-             render action: "index"
-         end
-      end
+   
 end
